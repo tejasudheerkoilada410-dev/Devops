eaa3dccdc440459524c8a82b58368e9d91c2a86e
Alla_Vyjayanthi <vyjayanthialla@gmail.com>
2025-12-28 16:54:52 +0530

    Initial project setup with env configs and docker

---
+++ app/app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {
+        "message": "Hello from DevOps App 🚀",
+        "environment": os.getenv("ENV_NAME"),
+        "debug": os.getenv("DEBUG"),
+        "log_level": os.getenv("LOG_LEVEL"),
+        "database": os.getenv("DB_URL")
+    }
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 5000)),
+        debug=os.getenv("DEBUG") == "true"
+    )
